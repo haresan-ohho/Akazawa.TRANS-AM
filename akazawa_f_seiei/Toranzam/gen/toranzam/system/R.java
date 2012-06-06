d6f44a5d47e53648c474417553b04ce6e6c8dd28
Takatoshi Hareyama <haresan.ohho@gmail.com>
2012-06-06 12:12:55 +0900

    2012/06/06_akazawa.f.seiei

--- akazawa_f_seiei/Toranzam/gen/toranzam/system/R.java
+++ akazawa_f_seiei/Toranzam/gen/toranzam/system/R.java
@@ -17,20 +17,23 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ha=0x7f020000;
-        public static final int haya=0x7f020001;
-        public static final int hayaruo=0x7f020002;
-        public static final int ho=0x7f020003;
-        public static final int hoho=0x7f020004;
-        public static final int icon=0x7f020005;
+        public static final int abe=0x7f020000;
+        public static final int ha=0x7f020001;
+        public static final int haya=0x7f020002;
+        public static final int hayaruo=0x7f020003;
+        public static final int ho=0x7f020004;
+        public static final int hoho=0x7f020005;
+        public static final int icon=0x7f020006;
     }
     public static final class id {
-        public static final int firstlayout=0x7f060001;
-        public static final int fivelayout=0x7f060005;
-        public static final int flipper=0x7f060000;
-        public static final int foralayout=0x7f060004;
-        public static final int secondlayout=0x7f060002;
-        public static final int thirdlayout=0x7f060003;
+        public static final int button_reference=0x7f060000;
+        public static final int firstlayout=0x7f060002;
+        public static final int fivelayout=0x7f060006;
+        public static final int flipper=0x7f060001;
+        public static final int foralayout=0x7f060005;
+        public static final int secondlayout=0x7f060003;
+        public static final int sixlayout=0x7f060007;
+        public static final int thirdlayout=0x7f060004;
     }
     public static final class layout {
         public static final int first=0x7f030000;
@@ -38,7 +41,8 @@ public final class R {
         public static final int fora=0x7f030002;
         public static final int main=0x7f030003;
         public static final int second=0x7f030004;
-        public static final int third=0x7f030005;
+        public static final int six=0x7f030005;
+        public static final int third=0x7f030006;
     }
     public static final class string {
         public static final int activity_animation_duration=0x7f050003;
